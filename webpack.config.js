9d19567cfdfa0b83056dc7a9499d215c710eb016
creayoon <Jyoon@olmangnim.local>
2017-05-08 21:25:30 +0900

    webpack server work & live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
 
@@ -6,12 +8,13 @@ module.exports = {
         filename: 'bundle.js'
     },
 
-    devServer: {
-        inline: true,
-        port: 7777,
-        contentBase: __dirname + '/public'
-    },
+    // devServer: {
+    //     inline: true,
+    //     port: 7777,
+    //     contentBase: __dirname + '/public'
+    // },
 
+    // setting env.
     module:
     {
         loaders: [
@@ -25,5 +28,16 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+
+    // make bundle file & livereload
+    plugins: [
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development, 
+            // ./public directory is being served 
+            host: 'localhost',
+            port: 7777,
+            server: { baseDir: ['public'] }
+        })
+    ]
 };
